de136b3acbc667efd28b3baec84cf5e3f68996a2
Madeleine Woodbury <mail@woodburycode.com>
2022-06-07 15:08:48 +0200

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
-  return <h1>Hello</h1>;
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import { AiFillCode, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <h1>
+            <a className='brand' href='/'>
+              <AiFillCode aria-hidden='true' className='icon' />
+              <span>Madeleine Woodbury</span>
+            </a>
+          </h1>
+        </li>
+        <li>
+          <a href='#projects'>Projects</a>
+        </li>
+        <li>
+          <a href='about.html'>About</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+        <li>
+          <a href='www.linkedin.com'>
+            <AiFillLinkedin aria-hidden='true' className='icon' />
+            <span className='sr-only'>LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a href='www.github.com'>
+            <AiFillGithub aria-hidden='true' className='icon' />
+            <span className='sr-only'>Github</span>
+          </a>
+        </li>
+        <li>
+          <a href='#resume' className='btn'>
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
