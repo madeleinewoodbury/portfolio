b2aeea3bed664e2002c867624c5ce1d04e86d88d
Madeleine Woodbury <mail@woodburycode.com>
2022-06-13 18:52:04 +0200

    add styling to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,25 @@
+import image from '../img/about.png';
+
 const About = () => {
   return (
     <section id='about'>
       <div className='container'>
-        <h2>About Me</h2>
+        <article>
+          <div className='text'>
+            <h2>About Me</h2>
+            <div className='info'>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis doloremque quaerat labore? Perspiciatis tenetur,
+                similique ad libero maiores velit illo eligendi necessitatibus.
+                Maiores neque vero modi, repudiandae quae velit reiciendis.
+              </p>
+            </div>
+          </div>
+          <div className='image'>
+            <img src={image} alt='Profile of me' />
+          </div>
+        </article>
       </div>
     </section>
   );
