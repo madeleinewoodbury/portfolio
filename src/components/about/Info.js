7d1f070eae759181a6326af6c30128fb4c95415d
Madeleine Woodbury <mail@woodburycode.com>
2022-06-13 19:40:57 +0200

    move about section to Info component

---
+++ src/components/about/Info.js
@@ -0,0 +1,28 @@
+import image from '../../img/about.png';
+
+const Info = () => {
+  return (
+    <section id='about'>
+      <div className='container'>
+        <article>
+          <div className='text'>
+            <h2>About Me</h2>
+            <div className='info'>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis doloremque quaerat labore? Perspiciatis tenetur,
+                similique ad libero maiores velit illo eligendi necessitatibus.
+                Maiores neque vero modi, repudiandae quae velit reiciendis.
+              </p>
+            </div>
+          </div>
+          <div className='image'>
+            <img src={image} alt='Profile of me' />
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Info;
